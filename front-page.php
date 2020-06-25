015cb61a690a79b4dd742314c092e93384046792
Shintaro Suka <suka0624suka@gmail.com>
2020-06-25 22:43:47 +0900

    フロントページ 対応(一部)

--- front-page.php
+++ front-page.php
@@ -1,3 +1,11 @@
 <?php get_header() ?>
-<?php
-get_footer();
+
+<main class="main">
+  <article class="article">
+    <?php if(have_posts()): while(have_posts()): the_post();?>
+    <?php the_content(); ?>
+    <?php endwhile; endif; ?>
+  </article>
+</main>
+
+<?php get_footer();
